a18a14cbc850f09e2927911026bbfce3af7a4a19
mattiaS <mattia@3doers.it>
2016-09-09 19:23:54 +0100

    small work on navbar

--- animation.js
+++ animation.js
@@ -7,9 +7,7 @@ $('a').click(function(){
 
 $(document).ready(function () {
     $('.nav li a').click(function(e) {
-
         $('.nav li').removeClass('active');
-
         var $parent = $(this).parent();
         if (!$parent.hasClass('active')) {
             $parent.addClass('active');
